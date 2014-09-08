877f88fe420a559e87ed687ac08c88134684c840
Josh <rkb.josh@gmail.com>
2014-09-07 21:52:44 -0400

    First commit

---
+++ lib/bounding_box.rb
@@ -0,0 +1,45 @@
+class BoundingBox
+attr_accessor :top, :bottom, :left, :right, :width, :height
+
+  def initialize(top, bottom, left, right)
+    @top = top
+    @bottom = bottom
+    @left = left
+    @right = right
+
+  end
+
+  def width
+    @width = 25.0
+  end
+
+  def height
+    @height = 50.0
+  end
+
+  def left
+    left_edge = 5.0
+  end
+
+  def right
+    right_edge = 30.0
+  end
+
+  def top
+    top_edge = 60.0
+  end
+
+  def bottom
+    bottom_edge = 10.0
+  end
+
+  def contains_point?(x, y)
+    #if x and y are in the box, evaluate to true
+
+    if x <= @height && y >= @width
+      return true
+    else
+      false
+    end
+  end
+end
